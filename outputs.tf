defbb8dd066d5eeda09de1768949cac398cb96da
damianbaldi <damianbaldi@gmail.com>
2023-03-07 18:16:23 -0300

    outputs.rf validate fix

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "instance_public_ip" {
     description = "instance public ip"
-    value = aws_instance.my_terratest.public.ip
+    value = aws_instance.my_terratest.public_ip
 }
 
 output "instance_public_dns" {
     description = "instance public dns"
-    value = aws_instance.my_terratest.public.dns
+    value = aws_instance.my_terratest.public_dns
 }
